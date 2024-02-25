0a72dfe4c1c393f94fc37fb72e674dd93cbca0bd
Juan Cruz Di Vanni <juancruzdivanni@gmail.com>
2024-02-24 21:12:00 -0300

    Creando objetos

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-let respuesta = prompt("Bienvenido/a a GO KARTS, desea jugar? ESC para salir")
+const pista =  {
+    cantidadVueltas: 5,
+    ubicacion: 'Buenos Aires',
+    dificultad: 'Dificil'
+}
+console.log(pista);
 
+const kart = {
+    color: 'verde',
+    motor: '100cc',
+    ruedas: 4
+}
+console.log(kart);
 
-while(respuesta != 'ESC'){
+let respuesta = prompt("Bienvenido/a a GO KARTS, desea jugar? escribir SI para empezar o NO para salir")
 
+while(respuesta === 'SI'){
     const velocidad1 = Math.floor(Math.random() * (10 + 1));
     const velocidad2 = Math.floor(Math.random() * (10 + 1));
     function correr(){
@@ -12,11 +24,11 @@ while(respuesta != 'ESC'){
             alert("¡Ganador Kart 2!")
         }else if(velocidad1 == velocidad2){
             alert("¡Es un empate!")
-        }
-                
+        }                
     }
 
-    const kart = prompt("Seleccione kart preferido: \n -Kart 1 \n -Kart 2" )
+    const kart = prompt("Seleccione kart preferido escribiendo 1 o 2: \n -Kart 1 \n -Kart 2" )
+    if((kart == '1') || (kart == '2')){
     switch(kart){
         case '1':
             alert("Seleccionaste Kart 1, ¡Hora de correr!")
@@ -26,10 +38,11 @@ while(respuesta != 'ESC'){
             alert("Seleccionaste Kart 2, ¡Hora de correr!")
             correr();
             break;
-
+    }
+    }else{
+        alert("Favor de seleccionar un número de kart correcto")
     }
 
-
-    respuesta = prompt("Bienvenido/a a GO KARTS, desea jugar? ESC para salir")
+    respuesta = prompt("¿Desea seguir jugando? escribir SI para empezar o NO para salir")
 
 }
